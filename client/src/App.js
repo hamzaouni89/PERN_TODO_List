ccdf461916144f13e66879fbb39a0f261de3a655
hamzaouni89 <hamzaouni@gmail.com>
2020-05-09 14:30:38 +0100

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React ,{ Fragment } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+import InputTodo from './components/InputTodo';
+import ListTodo from './components/ListTodos';
+function App() {
+  return (
+      <Fragment>
+        <div className="container">
+          <InputTodo/>
+          <ListTodo/>
+        </div>
+      </Fragment>
+  )
+}
+
+export default App;
